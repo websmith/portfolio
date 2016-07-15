421dcf08633fccdda7ca06d75d92abae14058023
Nate Cornelius <natecornelius@Nates-MacBook-Pro-2.local>
2016-07-15 09:30:00 -0500

    add lightbox images

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('cgPortfolio', ['ngRoute', 'portfolioControllers']);
+var app = angular.module('ncPortfolio', ['ngRoute', 'portfolioControllers']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/', {
